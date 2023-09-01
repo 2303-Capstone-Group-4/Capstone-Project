435271a36579b4273d80e0ffd09162c9ba9d68bb
Tom Warner <tommwarner@gmail.com>
2023-08-31 21:31:22 -0400

    attempting to clean frontend of framework

--- client/index.js
+++ client/index.js
@@ -1,16 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import {Provider} from 'react-redux'
-import {Router} from 'react-router-dom'
-import history from './history'
-import store from './store'
-import App from './App'
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
 
-ReactDOM.render(
-  <Provider store={store}>
-    <Router history={history}>
-      <App />
-    </Router>
-  </Provider>,
-  document.getElementById('app')
-)
+const root = createRoot(document.getElementById('root'));
+
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
