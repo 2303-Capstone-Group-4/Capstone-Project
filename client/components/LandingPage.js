a66c36cecfc1498770fada4a35bae9b445252433
Tom Warner <tommwarner@gmail.com>
2023-09-05 20:50:35 -0400

    got framework to be functional

--- client/components/LandingPage.js
+++ client/components/LandingPage.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { motion, useMotionValue, useTransform } from 'framer-motion';
 
 const LandingPage = () => {
   return (
     <div>
-      <h3>Welcome, {username}</h3>
+      <h3>Welcome, newUser</h3>
     </div>
   );
 };
--- client/index.js
+++ client/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 
-const root = createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById('app'));
 
 root.render(
   <BrowserRouter>
