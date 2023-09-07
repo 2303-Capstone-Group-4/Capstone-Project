70eaf6c9374a08fd9b50a8d3198e4a51bb3d9f1c
Michael <mrmchughes27@gmail.com>
2023-09-06 21:42:21 -0400

    got a basic gameBoard working

--- client/components/GameBoard.js
+++ client/components/GameBoard.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import { motion, useMotionValue, useTransform } from 'framer-motion';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
 
 const GameBoard = () => {
+  {/* Thinking a 10 x grid, block out the square we aren't using */}
+
+  {/* for (let i = 0; i < 60; i++) {
+          
+    } 
+  */}
+
   return (
     <div>
       <h3>This is the GameBoard</h3>
+
+      <Box sx={{ flexGrow: 1}}>
+      <Grid container spacing={1} columns={10} align="center">
+        {Array.from(Array(60)).map((_, index) => (
+          <Grid xs={1} key={index}>
+            <p>Space {index+1}</p>
+          </Grid>
+        ))}
+      </Grid>
+      </Box>
     </div>
   );
 };
