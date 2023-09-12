50b53275efd4a8774314b17133eb7aa285080de7
Michael <mrmchughes27@gmail.com>
2023-09-11 21:30:00 -0400

    got the key to work

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 
 const Navbar = () => (
-    <nav>
-      <div>
+  <Box>
+    <AppBar position="static">
+      <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
         <Link to="/">Home</Link>
-      </div>
-    </nav>
+      </Typography>
+    </AppBar>
+  </Box>
+    // <nav>
+    //   <div>
+    //     <Link to="/">Home</Link>
+    //   </div>
+    // </nav>
 );
 
 export default Navbar;
