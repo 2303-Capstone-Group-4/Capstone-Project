73d73b0690ed0ec1126ffb823bf50e7db4210d3b
Yaseen Jeber <yaseenj96@gmail.com>
2023-09-25 20:37:25 -0500

    Further debugged RadioGroup labels in Quizes

--- client/components/QuizPopup1.js
+++ client/components/QuizPopup1.js
@@ -1,21 +1,21 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Modal from '@mui/material/Modal';
-import Radio from '@mui/material/Radio';
-import RadioGroup from '@mui/material/RadioGroup';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import FormControl from '@mui/material/FormControl';
-import FormLabel from '@mui/material/FormLabel';
-import { useDispatch, useSelector } from 'react-redux';
-import { setTier1Comp } from '../store';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Modal from "@mui/material/Modal";
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/RadioGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormControl from "@mui/material/FormControl";
+import FormLabel from "@mui/material/FormLabel";
+import { useDispatch, useSelector } from "react-redux";
+import { setTier1Comp } from "../store";
 
 const QuizPopup1 = () => {
   const [open, setOpen] = React.useState(true);
   const [answers, setAnswers] = React.useState({
-    question1: '',
-    question2: '',
-    question3: '',
+    question1: "",
+    question2: "",
+    question3: "",
   });
   const [submitted, setSubmitted] = React.useState(false);
   const handleOpen = () => setOpen(true);
@@ -24,85 +24,85 @@ const QuizPopup1 = () => {
   const dispatch = useDispatch();
 
   React.useEffect(() => {
-    console.log('HERE IS USE EFFECT being called');
-    console.log('Here are the answers', answers);
-    console.log('Here is info1', info1);
+    console.log("HERE IS USE EFFECT being called");
+    console.log("Here are the answers", answers);
+    console.log("Here is info1", info1);
 
     if (
-      answers.question1 !== '' &&
-      answers.question2 !== '' &&
-      answers.question3 !== ''
+      answers.question1 !== "" &&
+      answers.question2 !== "" &&
+      answers.question3 !== ""
     ) {
       if (info1.Quiz?.Answers) {
-        console.log('First if statement');
+        console.log("First if statement");
         if (
           answers.question1 === info1.Quiz.Answers[0] &&
           answers.question2 === info1.Quiz.Answers[1] &&
           answers.question3 === info1.Quiz.Answers[2]
         ) {
-          alert('Correct, you may now proceed!');
+          alert("Correct, you may now proceed!");
           handleClose();
           dispatch(setTier1Comp(true));
         } else {
-          alert('Incorrect, you must try again!');
+          alert("Incorrect, you must try again!");
         }
       } else if (info1.Quiz?.Question1) {
         if (info1.Quiz.Question1.Answer) {
-          console.log('Second if statement');
+          console.log("Second if statement");
 
           if (
             answers.question1 === info1.Quiz.Question1.Answer &&
             answers.question2 === info1.Quiz.Question2.Answer &&
             answers.question3 === info1.Quiz.Question3.Answer
           ) {
-            alert('Correct, you may now proceed!');
+            alert("Correct, you may now proceed!");
             handleClose();
             dispatch(setTier1Comp(true));
           } else {
-            alert('Incorrect, you must try again!');
+            alert("Incorrect, you must try again!");
           }
         } else if (info1.Quiz?.Answer1) {
-          console.log('Third if statement');
+          console.log("Third if statement");
 
           if (
             answers.question1 === info1.Quiz.Answer1 &&
             answers.question2 === info1.Quiz.Answer2 &&
             answers.question3 === info1.Quiz.Answer3
           ) {
-            alert('Correct, you may now proceed!');
+            alert("Correct, you may now proceed!");
             handleClose();
             dispatch(setTier1Comp(true));
           } else {
-            alert('Incorrect, you must try again!');
+            alert("Incorrect, you must try again!");
           }
         }
       } else if (info1.Quiz?.Questions[0].Answer) {
-        console.log('test');
+        console.log("test");
         if (
           answers.question1 === info1.Quiz.Questions[0].Answer &&
           answers.question2 === info1.Quiz.Questions[1].Answer &&
           answers.question3 === info1.Quiz.Questions[2].Answer
         ) {
-          alert('Correct, you may now proceed!');
+          alert("Correct, you may now proceed!");
           handleClose();
           dispatch(setTier1Comp(true));
         } else {
-          alert('Incorrect, you must try again!');
+          alert("Incorrect, you must try again!");
         }
       }
     }
   }, [submitted]);
 
   const updateAnswers = (ev) => {
-    if (ev.target.name === ':r0:' || parseInt(ev.target.name[2]) % 3 === 0) {
+    if (ev.target.name === ":r0:" || parseInt(ev.target.name[2]) % 3 === 0) {
       setAnswers({ ...answers, question1: ev.target.value });
     } else if (
-      ev.target.name === ':r1:' ||
+      ev.target.name === ":r1:" ||
       (parseInt(ev.target.name[2]) - 1) % 3 === 0
     ) {
       setAnswers({ ...answers, question2: ev.target.value });
     } else if (
-      ev.target.name === ':r2:' ||
+      ev.target.name === ":r2:" ||
       (parseInt(ev.target.name[2]) - 2) % 3 === 0
     ) {
       setAnswers({ ...answers, question3: ev.target.value });
@@ -112,43 +112,37 @@ const QuizPopup1 = () => {
   return (
     <div>
       <Box textAlign="center">
-        <Button
-          onClick={handleOpen}
-          variant="contained"
-        >
+        <Button onClick={handleOpen} variant="contained">
           Re-Open Quiz 1
         </Button>
       </Box>
-      <Modal
-        open={open}
-        onClose={handleClose}
-      >
+      <Modal open={open} onClose={handleClose}>
         <Box
           sx={{
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            margin: 'auto',
-            flexWrap: 'no-wrap',
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            margin: "auto",
+            flexWrap: "no-wrap",
             mt: 10,
             width: 500,
-            height: '80%',
-            bgcolor: 'background.paper',
-            zIndex: 'modal',
-            fontSize: '1rem',
-            fontWeight: '500',
-            border: '2px solid #000',
+            height: "80%",
+            bgcolor: "background.paper",
+            zIndex: "modal",
+            fontSize: "1rem",
+            fontWeight: "500",
+            border: "2px solid #000",
             boxShadow: 24,
-            borderRadius: '15px',
+            borderRadius: "15px",
             p: 4,
-            overflowY: 'scroll',
+            overflowY: "scroll",
           }}
         >
           <form
             onSubmit={(ev) => {
               ev.preventDefault();
               console.log(
-                'BUTTON SUBMIT HAS BEEN CLICKED, here is submit:',
+                "BUTTON SUBMIT HAS BEEN CLICKED, here is submit:",
                 submitted
               );
               if (submitted) {
@@ -170,7 +164,7 @@ const QuizPopup1 = () => {
                           <FormLabel>
                             {question.Question ? question.Question : question}
                           </FormLabel>
-                          <RadioGroup>
+                          <RadioGroup name={`:r${index}:`}>
                             {info1.Quiz.Options
                               ? info1.Quiz.Options[index]
                                 ? info1.Quiz.Options[index].map((option) => {
@@ -236,7 +230,7 @@ const QuizPopup1 = () => {
                             ? info1.Quiz.Question1.Question
                             : info1.Quiz.Question1}
                         </FormLabel>
-                        <RadioGroup>
+                        <RadioGroup name=":r0:">
                           {info1.Quiz.Question1.Options
                             ? info1.Quiz.Question1.Options.map(
                                 (option, index2) => (
@@ -272,7 +266,7 @@ const QuizPopup1 = () => {
                             ? info1.Quiz.Question2.Question
                             : info1.Quiz.Question2}
                         </FormLabel>
-                        <RadioGroup>
+                        <RadioGroup name=":r1:">
                           {info1.Quiz.Question2.Options
                             ? info1.Quiz.Question2.Options.map(
                                 (option, index2) => (
@@ -308,7 +302,7 @@ const QuizPopup1 = () => {
                             ? info1.Quiz.Question3.Question
                             : info1.Quiz.Question3}
                         </FormLabel>
-                        <RadioGroup>
+                        <RadioGroup name=":r2:">
                           {info1.Quiz.Question3.Options
                             ? info1.Quiz.Question3.Options.map(
                                 (option, index2) => (
